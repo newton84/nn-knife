14eb8115068c1c2538c3e58c55b79f437edc9193
马顿 <madun8@163.com>
2025-03-29 17:05:02 +0800

    修改

--- utils/gf_utils.go
+++ utils/gf_utils.go
@@ -13,9 +13,9 @@ func GetHtml(s *ghttp.Server, relativePath string, content string) {
 	rs := []byte(content)
 	s.BindHandler(relativePath, func(r *ghttp.Request) {
 		r.Response.Status = http.StatusOK
-		r.Header.Add("content-type", "text/html;charset=UTF-8")
-		r.Header.Add("content-length", strconv.Itoa(len(rs)))
-		r.Header.Add("connection", "keep-alive")
+		r.Response.Header().Add("content-type", "text/html;charset=UTF-8")
+		r.Response.Header().Add("content-length", strconv.Itoa(len(rs)))
+		r.Response.Header().Add("connection", "keep-alive")
 		_, err := r.Response.BufferWriter.Write(rs)
 		if nil != err {
 			log.Fatal(err)
@@ -30,9 +30,9 @@ func GetJson(s *ghttp.Server, relativePath string, json string) {
 	s.BindHandler(relativePath, func(r *ghttp.Request) {
 		rs := []byte(json)
 		r.Response.Status = http.StatusOK
-		r.Header.Add("content-type", "text/html;charset=UTF-8")
-		r.Header.Add("content-length", strconv.Itoa(len(rs)))
-		r.Header.Add("connection", "keep-alive")
+		r.Response.Header().Add("content-type", "text/json;charset=UTF-8")
+		r.Response.Header().Add("content-length", strconv.Itoa(len(rs)))
+		r.Response.Header().Add("connection", "keep-alive")
 		_, err := r.Response.BufferWriter.Write(rs)
 		if nil != err {
 			log.Fatal(err)
@@ -50,9 +50,9 @@ func GetJs(s *ghttp.Server, relativePath string, hexContent string) {
 	s.BindHandler(relativePath, func(r *ghttp.Request) {
 
 		r.Response.Status = http.StatusOK
-		r.Header.Add("content-type", "text/html;charset=UTF-8")
-		r.Header.Add("content-length", strconv.Itoa(len(rs)))
-		r.Header.Add("connection", "keep-alive")
+		r.Response.Header().Add("content-type", "text/javascript;charset=UTF-8")
+		r.Response.Header().Add("content-length", strconv.Itoa(len(rs)))
+		r.Response.Header().Add("connection", "keep-alive")
 		_, err := r.Response.BufferWriter.Write(rs)
 		if nil != err {
 			log.Fatal(err)
@@ -67,14 +67,15 @@ func GetCss(s *ghttp.Server, relativePath string, content string) {
 	s.BindHandler(relativePath, func(r *ghttp.Request) {
 		rs := []byte(content)
 		r.Response.Status = http.StatusOK
-		r.Header.Add("content-type", "text/html;charset=UTF-8")
-		r.Header.Add("content-length", strconv.Itoa(len(rs)))
+		r.Response.Header().Add("content-type", "text/css;charset=UTF-8")
+		r.Response.Header().Add("content-length", strconv.Itoa(len(rs)))
 		r.Header.Add("connection", "keep-alive")
 		_, err := r.Response.BufferWriter.Write(rs)
 		if nil != err {
 			log.Fatal(err)
 			return
 		}
+		r.Response.BufferWriter.Flush()
 	})
 
 }
@@ -89,8 +90,8 @@ func GetOther(s *ghttp.Server, relativePath string, hexContent string) {
 	s.BindHandler(relativePath, func(r *ghttp.Request) {
 
 		r.Response.Status = http.StatusOK
-		r.Header.Add("content-length", strconv.Itoa(len(rs)))
-		r.Header.Add("connection", "keep-alive")
+		r.Response.Header().Add("content-length", strconv.Itoa(len(rs)))
+		r.Response.Header().Add("connection", "keep-alive")
 		_, err := r.Response.BufferWriter.Write(rs)
 		if nil != err {
 			log.Fatal(err)
